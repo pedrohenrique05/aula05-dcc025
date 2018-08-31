dceaf0e271813bd4851daf633e8f8501acb4a868
pedro <pedro@172.18.133.64>
2018-08-31 19:34:37 -0300

    aula 08

--- Aula08/src/aula08/Aula08.java
+++ Aula08/src/aula08/Aula08.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aula06;
+package aula08;
 
 /**
  *
  * @author pedro
  */
-public class Aula06 {
+public class Aula08 {
 
     /**
      * @param args the command line arguments
