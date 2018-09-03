574dde9168b3a3b590c32fe020fd8c9f45956f58
pedro <pedro@172.18.133.64>
2018-09-03 15:53:39 -0300

    Retorna um valor aleatorio de 1 a 6.

---
+++ Aula08/src/aula08/Pontuacao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula08;
+
+/**
+ *
+ * @author pedro
+ */
+public class Pontuacao {
+    private int posVet = 0;
+    private int aux = 0;
+    public int retornaPont(int [] vet){
+      return 0;
+    }
+}
