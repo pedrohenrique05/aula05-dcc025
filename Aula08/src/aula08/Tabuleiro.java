609cfc8bf8b7ba2e159166c77726aaf19b522083
pedro <pedro@172.18.133.64>
2018-09-10 20:39:36 -0300

    Adicionando pontuação no tabuleiro

--- Aula08/src/aula08/Tabuleiro.java
+++ Aula08/src/aula08/Tabuleiro.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package aula08.pkg1;
+package aula08;
 
 /**
  *
@@ -18,7 +18,30 @@ public class Tabuleiro {
      *  seta a pontuação
      */
    public void pontuacao(int [] tb){
-       
+       int as = 0, duas = 0, tres = 0 , quadra = 0, quina = 0, sena = 0;
+       for(int aux  = 0 ; aux < 5 ; aux++){
+           switch(tb[aux]){
+               case 1:
+                   as++;
+                   break;
+               case 2:
+                   duas++;
+                   break;
+               case 3:
+                   tres++;
+                   break;
+               case 4:
+                   quadra++;
+                   break;
+               case 5:
+                   quina++;
+                   break;
+               case 6:
+                   sena++;
+                   break;
+           }
+           if()
+       }
    }
     public void setPontRodada(int ind, int val){
         this.tabuleiro[ind] = val;
